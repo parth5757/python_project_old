3fe7ba7abfa92878602591933bfea7de5e112fb6
Parth Thakkar <60839082+parth5757@users.noreply.github.com>
2023-08-06 14:52:23 +0530

    Delete 21_FEB directory

--- 21_FEB/name_error.py
+++
@@ -1,5 +0,0 @@
-# name error
-name = "parth" 
-# print(age) # here it return the name error becusase it has no age variavle is def
-age = 21
-print(age) # if you comment third line than it will give you proper output 21
